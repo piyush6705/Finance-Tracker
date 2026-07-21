a8ef4c82893afd7ff2c2d161e3b9cc7c48be66cf
piyush6705 <sharmapiyush6705@gmail.com>
2026-07-22 02:38:04 +0530

    installed chats and now started working on expensepiechart

---
+++ finance-tracker/src/components/Charts/ExpensePieChart.jsx
@@ -0,0 +1,11 @@
+import "./ExpensePieChart.css";
+
+function ExpensePieChart() {
+  return (
+    <div className="chart-card">
+      <h2>Expense Breakdown</h2>
+    </div>
+  );
+}
+
+export default ExpensePieChart;
