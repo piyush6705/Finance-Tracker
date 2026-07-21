16390c77bcb143ff7fd53cdde43306e73b439e6d
piyush6705 <sharmapiyush6705@gmail.com>
2026-07-22 01:06:04 +0530

    download icon package and created dashboard.css and added dumy data in dashboard.css

--- finance-tracker/src/pages/Dashboard/Dashboard.jsx
+++ finance-tracker/src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,52 @@
 
 import Layout from "../../components/Layout/Layout";
+import SummaryCard from "../../components/Dashboard/SummaryCard";
+import "./Dashboard.css";
+import {
+  FaArrowUp,
+  FaArrowDown,
+  FaWallet,
+  FaPiggyBank,
+} from "react-icons/fa";
+
 
 function Dashboard() {
   return (
     <Layout>
-      <h1>Dashboard</h1>
-    </Layout>
+      <div className="dashboard">
+        <div className="cards">
+
+          <SummaryCard
+            title="Income"
+            amount={50000}
+            icon={<FaArrowUp />}
+            color="#4CAF50"
+          />
+
+          <SummaryCard
+            title="Expenses"
+            amount={25000}
+            icon={<FaArrowDown />}
+            color="#F44336"
+          />
+
+          <SummaryCard
+            title="Balance"
+            amount={25000}
+            icon={<FaWallet />}
+            color="#2196F3"
+          />
+
+          <SummaryCard
+            title="Savings"
+            amount={10000}
+            icon={<FaPiggyBank />}
+            color="#FF9800"
+          />
+
+        </div>
+      </div>
+     </Layout>
   );
 }
 
