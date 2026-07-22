16ef1cbb0d78498a0f06039ea111ce599468665c
piyush6705 <sharmapiyush6705@gmail.com>
2026-07-23 01:31:45 +0530

    add Analytics page layout and styles with SummaryCards, TopCategories, and Analytitis and rendered all it into  it

--- finance-tracker/src/pages/Analytics/Analytics.jsx
+++ finance-tracker/src/pages/Analytics/Analytics.jsx
@@ -1,15 +1,47 @@
+import "./Analytics.css";
+
 import Layout from "../../components/Layout/Layout";
+
+import SummaryCards from "../../components/Analytics/SummaryCards/SummaryCards";
+import TopCategories from "../../components/Analytics/TopCategories/TopCategories";
+import AnalyticsStats from "../../components/Analytics/AnalyticsStats/AnalyticsStats";
+
 import IncomeExpenseChart from "../../components/Charts/IncomeExpenseChart/IncomeExpenseChart";
-
 import ExpensePieChart from "../../components/Charts/ExpensePieChart/ExpensePieChart";
-
 import MonthlyExpenseChart from "../../components/Charts/MonthlyExpenseChart/MonthlyExpenseChart";
 
 function Analytics() {
   return (
     <Layout>
-      <h1>Analytics</h1>
-      
+
+      <div className="analytics-page">
+
+        <div className="analytics-header">
+          <h1>Analytics</h1>
+          <p>
+            Get insights into your income, expenses,
+            and overall financial performance.
+          </p>
+        </div>
+
+        <SummaryCards />
+
+        <div className="analytics-charts">
+
+          <IncomeExpenseChart />
+
+          <ExpensePieChart />
+
+          <MonthlyExpenseChart />
+
+        </div>
+
+        <TopCategories />
+
+        <AnalyticsStats />
+
+      </div>
+
     </Layout>
   );
 }
