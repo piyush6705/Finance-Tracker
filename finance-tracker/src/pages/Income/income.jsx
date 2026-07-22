900d1d5159391fa643fd1a8d2f4aa571b6e08abe
piyush6705 <sharmapiyush6705@gmail.com>
2026-07-22 19:47:54 +0530

    rendering pages to the income page

--- finance-tracker/src/pages/Income/income.jsx
+++ finance-tracker/src/pages/Income/income.jsx
@@ -1,9 +1,18 @@
 import Layout from "../../components/Layout/Layout";
+import SearchIncome from "../../components/Income/SearchIncome/SearchIncome";
+import IncomeList from "../../components/Income/IncomeList/IncomeList";
+import AddIncomeModal from "../../components/Income/AddIncomeModal/AddIncomeModal";
+
+import "./Income.css";
 
 function Income() {
   return (
     <Layout>
-      <h1>Income</h1>
+      <SearchIncome />
+
+      <IncomeList />
+
+      <AddIncomeModal />
     </Layout>
   );
 }
