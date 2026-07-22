877b3557ba55a4687f9273b8e78494192653511a
piyush6705 <sharmapiyush6705@gmail.com>
2026-07-22 23:48:37 +0530

    done with expense list

---
+++ finance-tracker/src/components/Expense/ExpenseList/ExpenseList.jsx
@@ -0,0 +1,55 @@
+import ExpenseCard from "../ExpenseCard/ExpenseCard";
+import "./ExpenseList.css";
+
+const expenses = [
+  {
+    id: 1,
+    title: "Groceries",
+    amount: 2500,
+    category: "Food",
+    date: "22 Jul 2026",
+  },
+  {
+    id: 2,
+    title: "House Rent",
+    amount: 15000,
+    category: "Rent",
+    date: "20 Jul 2026",
+  },
+  {
+    id: 3,
+    title: "Movie",
+    amount: 800,
+    category: "Entertainment",
+    date: "18 Jul 2026",
+  },
+];
+
+function ExpenseList({ search }) {
+
+  const filteredExpense = expenses.filter((expense)=>
+    expense.title
+      .toLowerCase()
+      .includes(search.toLowerCase())
+  );
+
+  return (
+    <div className="expense-list">
+
+      {filteredExpense.map((expense)=>(
+        <ExpenseCard
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          category={expense.category}
+          date={expense.date}
+          onEdit={() => alert(`Edit ${expense.title}`)}
+          onDelete={() => alert(`Delete ${expense.title}`)}
+        />
+      ))}
+
+    </div>
+  );
+}
+
+export default ExpenseList;
